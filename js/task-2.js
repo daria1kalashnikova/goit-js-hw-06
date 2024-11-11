d57bcf658edef348181ae6a35ba9d214e706cbee
daria1kalashnikova <dk.kalashnikova@gmail.com>
2024-11-12 00:27:39 +0200

    done

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,23 @@
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const index = this.#items.findIndex(
+      (el) => el.toLowerCase() === itemToRemove.toLowerCase()
+    );
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
